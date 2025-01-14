cc106d764e806bfa130c0ffa73a09fe88d56440e
shaikhazrath <shaikhazrathali123@gmail.com>
2025-01-14 23:49:50 +0530

    routing update

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -20,7 +20,7 @@ const AuthPage = () => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/app`
+          redirectTo: `/app`
         }
       })
 
